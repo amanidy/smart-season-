0d6645566b598e4c4784f83bf99b3f597e3ceb7a
amanidy <amaniarnold08@gmail.com>
2026-05-14 19:45:39 +0300

    Added dashboard side navbar

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,1 +1,10 @@
 //stats, overview rendering 
+
+
+console.log("Hello dashboard!");
+
+const sidebar = document.getElementById('side-nav');
+
+document.getElementById('menu-btn').onclick = () => sidebar.classList.toggle('active');
+document.getElementById('close-btn').onclick = () => sidebar.classList.remove('active');
+
