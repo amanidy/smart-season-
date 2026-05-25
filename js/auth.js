af376b72e32feb82e6f02c6d95230cb6177e6212
amanidy <amaniarnold08@gmail.com>
2026-05-25 13:59:33 +0300

    added login authentication

--- js/auth.js
+++ js/auth.js
@@ -2,5 +2,30 @@
 
 import {users} from '../js/mockData.js';
 
+const errorMessage = document.getElementById('error-message');
+errorMessage.style.display = 'none';
 
-console.log(users);
+document.getElementById('user-info').addEventListener('submit',(e)=>{
+  e.preventDefault();
+  
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+  
+  
+  const user = users.find(u => u.email === email && u.password === password);
+  
+  if(user){
+    
+    errorMessage.style.display = 'none';
+    localStorage.setItem('currentUser',JSON.stringify({
+      id:user.id,
+      name:user.name,
+      role:user.role,
+    }))
+    
+    window.location.href ='dashboard.html'
+  } else{
+    errorMessage.style.display = 'block';
+  }
+  
+})
